34ac825b1013a87ac600f4be46c452faaec4924a
PockeyPepero <rhysinvillahermosa16@gmail.com>
2024-05-18 20:25:28 +0800

    fix: js script not working

--- public/js/script.js
+++ public/js/script.js
@@ -11,10 +11,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
         const htmlContent = localStorage.getItem("html_code");
         const cssContent = `<style>${localStorage.getItem("css_code")}</style>`;
-        const jsContent = `<script>${localStorage.getItem("js_code")}</script>`;
+        const jsContent = `<script>(function(){${localStorage.getItem(
+            "js_code"
+        )}})();</script>`;
+
+        // output.contentDocument.open();
+        // output.contentDocument.write(cssContent + htmlContent + jsContent);
+        // output.contentDocument.close();
 
         output.contentDocument.open();
-        output.contentDocument.write(htmlContent + cssContent + jsContent);
+        output.contentDocument.writeln(cssContent + htmlContent + jsContent);
         output.contentDocument.close();
     }
 
