e1b49e4513d9d03486cb4412b3e2287ba87c045b
Kevin Webster Revalde <kevinwebsterr2016@gmail.com>
2024-06-03 00:00:26 +0800

    fix error

--- resources/views/profile/card.blade.php
+++ resources/views/profile/card.blade.php
@@ -53,21 +53,24 @@
         </li>
     </ul>
 
-    @if ($user->id !== auth()->user()->id)
-        <div class="p-4 border-t mx-8 mt-2">
-            @if (Auth::user() && Auth::user()->follows($user))
-                <form action="{{ route('user.unfollow', $user) }}" method="POST">
-                    @csrf
-                    <button type="submit"
-                        class="w-1/2 block mx-auto rounded-full bg-red-900 hover:shadow-lg font-semibold text-white px-6 py-2">Unfollow</button>
-                </form>
-            @else
-                <form action="{{ route('user.follow', $user) }}" method="POST">
-                    @csrf
-                    <button type="submit"
-                        class="w-1/2 block mx-auto rounded-full bg-primaryColor hover:shadow-lg font-semibold text-white px-6 py-2">Follow</button>
-                </form>
-            @endif
-        </div>
-    @endif
+    @auth
+
+        @if ($user->id !== auth()->user()->id)
+            <div class="p-4 border-t mx-8 mt-2">
+                @if (Auth::user() && Auth::user()->follows($user))
+                    <form action="{{ route('user.unfollow', $user) }}" method="POST">
+                        @csrf
+                        <button type="submit"
+                            class="w-1/2 block mx-auto rounded-full bg-red-900 hover:shadow-lg font-semibold text-white px-6 py-2">Unfollow</button>
+                    </form>
+                @else
+                    <form action="{{ route('user.follow', $user) }}" method="POST">
+                        @csrf
+                        <button type="submit"
+                            class="w-1/2 block mx-auto rounded-full bg-primaryColor hover:shadow-lg font-semibold text-white px-6 py-2">Follow</button>
+                    </form>
+                @endif
+            </div>
+        @endif
+    @endauth
 </div>
